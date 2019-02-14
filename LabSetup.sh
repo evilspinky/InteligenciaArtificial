7ddfd308fa4b7421e28ac49682b334d95f01e50a
evilspinky <46283085+evilspinky@users.noreply.github.com>
2019-02-14 03:30:16 -0600

    Create LabSetup.sh

---
+++ LabSetup.sh
@@ -0,0 +1,4 @@
+#                                                 ----- Windows -----
+#En python3.exe instalar pip,tk y agregar variables de entorno
+#                                                 ------ Linux ------
+sudo apt-get install python3 python3-pip python3-tk
